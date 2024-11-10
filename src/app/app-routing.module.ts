f1f5ec6b5c3fe8f07c9f955cea8857ed14377ca7
akili sosa <akilisosa@gmail.com>
2024-11-10 15:11:32 -0600

    amplify community

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'main-page',
+    loadChildren: () => import('./main-page/main-page.module').then( m => m.MainPagePageModule)
   }
 ];
 
